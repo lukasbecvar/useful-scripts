bb81f4026d973cc5e2b8b9b3b292ab8ebf5e7b1e
Lukáš Bečvář <48027782+lordbecvold@users.noreply.github.com>
2024-09-05 20:36:34 +0200

    Update temp-throttle.sh

--- temp-throttle.sh
+++ temp-throttle.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 # Usage: temp_throttle.sh --max-temp=65 --max-freq=auto
-# USE CELSIUS TEMPERATURES.
 # Original script: https://github.com/Sepero/temp-throttle
+# USE CELSIUS TEMPERATURES
 
-# Credits
-# Wolfgang Ocker <weo AT weo1 DOT de> - Patch for unspecified cpu frequencies.
-
-# generic function for printing an error and exiting.
+# generic function for printing an error
 err_exit () {
     echo ""
     echo -e "\033[31m\033[1m[$(date '+%H:%M:%S')] error\e[0m: $@" 1>&2
@@ -55,28 +52,26 @@ if [ "$MAX_FREQ" = "auto" ]; then
     fi
 fi
 
-### START Initialize Global variables.
-
-# the frequency will increase when low temperature is reached.
+# the frequency will increase when low temperature is reached
 LOW_TEMP=$((MAX_TEMP - 5))
 
 CORES=$(nproc) # get number of CPU cores.
 echo -e "\033[33m\033[1mNumber of CPU cores detected: $CORES\033[0m"
-CORES=$((CORES - 1)) # subtract 1 from $CORES for easier counting later.
+CORES=$((CORES - 1)) # subtract 1 from $CORES for easier counting later
 
-# temperatures internally are calculated to the thousandth.
+# temperatures internally are calculated to the thousandth
 MAX_TEMP=${MAX_TEMP}000
 LOW_TEMP=${LOW_TEMP}000
 
 FREQ_FILE="/sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies"
 FREQ_MIN="/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_min_freq"
 
-# store available cpu frequencies in a space separated string FREQ_LIST.
+# store available cpu frequencies in a space separated string FREQ_LIST
 if [ -f $FREQ_FILE ]; then
-    # if $FREQ_FILE exists, get frequencies from it.
+    # if $FREQ_FILE exists, get frequencies from it
     FREQ_LIST=$(cat $FREQ_FILE | xargs -n1 | sort -g -r | xargs) || err_exit "Could not read available cpu frequencies from file $FREQ_FILE"
 elif [ -f $FREQ_MIN ]; then
-    # else if $FREQ_MIN exists, generate a list of frequencies starting from $FREQ_MIN.
+    # else if $FREQ_MIN exists, generate a list of frequencies starting from $FREQ_MIN
     FREQ_LIST=$(seq $MAX_FREQ -100000 $(cat $FREQ_MIN)) || err_exit "Could not compute available cpu frequencies"
 else
     err_exit "Could not determine available cpu frequencies"
@@ -84,10 +79,10 @@ fi
 
 FREQ_LIST_LEN=$(echo $FREQ_LIST | wc -w)
 
-# CURRENT_FREQ will save the index of the currently used frequency in FREQ_LIST.
+# CURRENT_FREQ will save the index of the currently used frequency in FREQ_LIST
 CURRENT_FREQ=2
 
-# this is a list of possible locations to read the current system temperature.
+# list of possible locations to read the current system temperature
 TEMPERATURE_FILES="
 /sys/class/thermal/thermal_zone0/temp
 /sys/class/thermal/thermal_zone1/temp
@@ -101,8 +96,8 @@ TEMPERATURE_FILES="
 null
 "
 
-# store the first temperature location that exists in the variable TEMP_FILE.
-# the location stored in $TEMP_FILE will be used for temperature readings.
+# store the first temperature location that exists in the variable TEMP_FILE
+# the location stored in $TEMP_FILE will be used for temperature readings
 for file in $TEMPERATURE_FILES; do
     TEMP_FILE=$file
     [ -f $TEMP_FILE ] && break
@@ -110,26 +105,22 @@ done
 
 [ "$TEMP_FILE" = "null" ] && err_exit "The location for temperature reading was not found."
 
-### END Initialize Global variables.
-
-### START define script functions.
-
-# set the maximum frequency for all cpu cores.
+# set the maximum frequency for all cpu cores
 set_freq () {
-    # from the string FREQ_LIST, we choose the item at index CURRENT_FREQ.
+    # from the string FREQ_LIST, we choose the item at index CURRENT_FREQ
     FREQ_TO_SET=$(echo $FREQ_LIST | cut -d " " -f $CURRENT_FREQ)
     echo $FREQ_TO_SET
     for i in $(seq 0 $CORES); do
-        # Try to set core frequency by writing to /sys/devices.
+        # Try to set core frequency by writing to /sys/devices
         { echo $FREQ_TO_SET 2> /dev/null > /sys/devices/system/cpu/cpu$i/cpufreq/scaling_max_freq; } ||
-        # Else, try to set core frequency using command cpufreq-set.
+        # Else, try to set core frequency using command cpufreq-set
         { cpufreq-set -c $i --max $FREQ_TO_SET > /dev/null; } ||
-        # Else, return error message.
+        # Else, return error message
         { err_exit "Failed to set frequency CPU core$i. Run script as Root user. Some systems may require to install the package cpufrequtils."; }
     done
 }
 
-# will reduce the frequency of cpus if possible.
+# reduce the frequency of cpus if possible
 throttle () {
     if [ $CURRENT_FREQ -lt $FREQ_LIST_LEN ]; then
         CURRENT_FREQ=$((CURRENT_FREQ + 1))
@@ -138,7 +129,7 @@ throttle () {
     fi
 }
 
-# will increase the frequency of cpus if possible.
+# increase the frequency of cpus if possible
 unthrottle () {
     if [ $CURRENT_FREQ -ne 1 ]; then
         CURRENT_FREQ=$((CURRENT_FREQ - 1))
@@ -152,18 +143,16 @@ get_temp () {
     TEMP=$(cat $TEMPERATURE_FILES 2>/dev/null | xargs -n1 | sort -g -r | head -1)
 }
 
-### END define script functions.
-
 echo -e "\033[33m\033[1mInitialize to max CPU frequency\033[0m"
 unthrottle
 
 # main loop
 while true; do
-    get_temp # gets the current temperature and set it to the variable TEMP.
-    if   [ $TEMP -gt $MAX_TEMP ]; then # throttle if too hot.
+    get_temp # gets the current temperature and set it to the variable TEMP
+    if   [ $TEMP -gt $MAX_TEMP ]; then # throttle if too hot
         throttle
-    elif [ $TEMP -le $LOW_TEMP ]; then # unthrottle if cool.
+    elif [ $TEMP -le $LOW_TEMP ]; then # unthrottle if cool
         unthrottle
     fi
-    sleep 1 # the amount of time between checking temperatures.
+    sleep 3 # the amount of time between checking temperatures
 done

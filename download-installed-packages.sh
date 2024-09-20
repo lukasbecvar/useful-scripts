e58ebffa39f7c9bc9e27d5d46764433715b9df9b
Lukáš Bečvář <48027782+lordbecvold@users.noreply.github.com>
2024-09-20 21:10:14 +0200

    Update download-installed-packages.sh

--- download-installed-packages.sh
+++ download-installed-packages.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
 
-# script for download all instaled deb packages (only for apt)
+# script for downloading all installed deb packages (only for apt)
 
 # clear console
 clear
 
-echo "Do you want download all instaled packages?\n"
+echo "Do you want to download all installed packages?"
 echo "[YES or NO]: "
 
 read selector
 
 case $selector in
-        yes|YES)
-		# get installed packages list
-		dpkg --get-selections > installed-packages.txt
+    yes|YES)
+        # get installed packages list
+        dpkg --get-selections > installed-packages.txt
 
-		# download all packages
-		sudo apt-get download $(awk '{print $1}' installed-packages.txt)
-        ;;
+        # download all packages
+        for package in $(awk '{print $1}' installed-packages.txt); do
+            echo "Downloading $package..."
+            sudo apt-get download "$package" 2>/dev/null
+            if [ $? -ne 0 ]; then
+                echo "Skipping $package, package not found."
+            fi
+        done
+    ;;
     no|NO)
-                echo "Process exited."
-        ;;
+        echo "Process exited."
+    ;;
     *)
-                echo "Your vote not found!\n"
+        echo "Your choice not found!"
     ;;
 esac
-

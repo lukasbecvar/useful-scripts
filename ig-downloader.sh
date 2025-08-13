b5531fa53da9a273f296680d26367c19a61d0266
Lukáš Bečvář <48027782+lukasbecvar@users.noreply.github.com>
2025-08-13 14:47:10 +0200

    Update ig-downloader.sh

--- ig-downloader.sh
+++ ig-downloader.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
+# usage bash ig-downloader.bash --full (full flag = download stories, highlights & tagged)
+
 # check if command is bash
 if [ -z "$BASH_VERSION" ]; then
-    echo "\e[31mPlease run this script with bash command.\e[0m"
+    echo -e "\e[31mPlease run this script with bash command.\e[0m"
     exit 1
+fi
+
+# extra flags for --full download mode
+EXTRA_FLAGS=""
+if [[ "$1" == "--full" ]]; then
+    EXTRA_FLAGS="--highlights --stories --tagged"
 fi
 
 # list of usernames to download
 usernames=(
-    "profile_1"
-    "profile_2"
+    "username1"
+    "username2"
+    "username3"
+    "username4"
 )
 
 # download process
 for profile in "${usernames[@]}"; do
     echo -e "\e[34mDownloading data from profile: \e[1m$profile\e[0m"
-    instaloader --no-videos --no-video-thumbnails --no-captions \
-                --no-metadata-json --no-compress-json --fast-update "$profile"
+    instaloader --no-videos --no-video-thumbnails --no-captions --no-metadata-json --no-compress-json \
+                --fast-update --load-cookies=firefox $EXTRA_FLAGS "$profile"
     echo -e "\e[32mFinished downloading: \e[1m$profile\e[0m"
 done
 
